f07f6f0e1608de1190fafa106209782fff3c96b0
Jimcl6 <jedllorente@gmail.com>
2025-12-19 11:51:55 +0800

    commit: con't revamping torque edit module

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     @routes
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    @vite(['../resources/css/app.css', '../resources/js/app.js'])
     @inertiaHead
 </head>
 <body>
