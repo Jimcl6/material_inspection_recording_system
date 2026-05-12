b9b7a04b8f907fe87acad0924e5b8fb35fda21d7
Jimcl6 <jedllorente@gmail.com>
2026-05-12 17:31:58 +0800

    commit: sidebar completion

--- resources/js/Layouts/AppLayout.vue
+++ resources/js/Layouts/AppLayout.vue
@@ -611,7 +611,7 @@ const mainMargin = computed(() => {
             <!-- Page content -->
             <main class="flex-1">
                 <div class="py-6">
-                    <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+                    <div class=" mx-auto px-4 sm:px-6 lg:px-8">
                         <slot />
                     </div>
                 </div>
