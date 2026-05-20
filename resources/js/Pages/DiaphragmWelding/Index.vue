571b87c3c4962dd173f74458246bb6d7cfac111d
Jimcl <81977373+Jimcl6@users.noreply.github.com>
2026-05-20 16:41:47 +0800

    commit: commented th class in Diaphragm Welding Index

--- resources/js/Pages/DiaphragmWelding/Index.vue
+++ resources/js/Pages/DiaphragmWelding/Index.vue
@@ -234,7 +234,7 @@ const statusClass = (status: string) => {
                                         <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">JO Number</th>
                                         <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Qty</th>
                                         <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Status</th>
-                                        <th class="px-6 py-3 text-right text-xs font-medium text-gray-500 uppercase tracking-wider">Actions</th>
+                                        <!-- <th class="px-6 py-3 text-right text-xs font-medium text-gray-500 uppercase tracking-wider">Actions</th> -->
                                     </tr>
                                 </thead>
                                 <tbody class="bg-white divide-y divide-gray-200">
