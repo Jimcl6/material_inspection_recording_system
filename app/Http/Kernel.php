ebaef2f2181b509ca98eeaae5115f3d895c7f042
Jimcl <81977373+Jimcl6@users.noreply.github.com>
2026-07-22 15:59:17 +0800

    Use framework-native CORS middleware

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -23,8 +23,6 @@ class Kernel extends HttpKernel
         \App\Http\Middleware\TrimStrings::class,
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
         \App\Http\Middleware\PreventSearchIndexing::class,
-        // \App\Http\Middleware\HandleCors::class,
-        // \Fruitcake\Cors\HandleCors::class,
     ];
 
     /**
